82585f5a55785db7219192c808aa8f19cae5041f
teicheld <teicheld@web.de>
2023-09-15 11:02:01 +0200

    Create mkp224_filterfile_homoglyph_generator.py

---
+++ mkp224_filterfile_homoglyph_generator.py
@@ -0,0 +1,40 @@
+import sys
+
+def generate_homoglyphs(word):
+    homoglyphs = {
+        'a': ['4'],
+        'b': ['8'],
+        'e': ['3'],
+        'g': ['9'],
+        'i': ['1', 'l'],
+        'l': ['1', 'i', '7'],
+        'o': ['0'],
+        's': ['5'],
+        't': ['7'],
+        'z': ['2'],
+        'p': ['q'],
+        'q': ['p']
+    }
+    
+    variations = [list(word)]
+    
+    for char in word:
+        if char.lower() in homoglyphs:
+            new_variations = []
+            for glyph in homoglyphs[char.lower()]:
+                for variation in variations:
+                    new_variation = variation.copy()
+                    new_variation[new_variation.index(char)] = glyph
+                    new_variations.append(new_variation)
+            variations.extend(new_variations)
+    
+    homoglyph_words = [''.join(variation) for variation in variations]
+    return homoglyph_words
+
+arguments = sys.argv[1:]
+
+for input_word in arguments:
+    homoglyphs = generate_homoglyphs(input_word)
+
+    for homoglyph in homoglyphs:
+        print(homoglyph)
